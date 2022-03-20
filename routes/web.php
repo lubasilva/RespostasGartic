b0167f08f848e0283c67fd688b89b1418faa470e
Lucas <lubapisilva@gmail.com>
2022-03-20 12:31:55 -0300

    add laravel

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\LayoutController;
+use App\Http\Controllers\ObjetoController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [LayoutController::class, 'index']);
+Route::get('/objetos', [ObjetoController::class, 'index']);
